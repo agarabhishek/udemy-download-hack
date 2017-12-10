49a0a801294e788fa55a43c7f30aac887e496751
Abhishek Agarwal <31480447+click4abhishek@users.noreply.github.com>
2017-12-10 18:08:29 +0530

    Update code.bat

--- code.bat
+++ code.bat
@@ -1,5 +1,5 @@
 pip install youtube-dl 
-cd ../../Users/Abhishek/Desktop/Vid 
+cd ../../Users/Abhishek/Desktop/Vid rem Replace this with your location
 youtube-dl -u "1234@gmail.com" -p "1234pass" https://www.udemy.com/learn-ethical/learn/v4/content 
 
 
