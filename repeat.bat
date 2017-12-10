9af77d565a47e229c14e9afb71732b74763c7c15
Abhishek Agarwal <31480447+click4abhishek@users.noreply.github.com>
2017-12-10 18:01:29 +0530

    Update repeat.bat

--- repeat.bat
+++ repeat.bat
@@ -1,7 +1,7 @@
 pip install youtube-dl 
 cd ../../Users/Abhishek/Desktop/Vid # Replace it with the location where the videos is to be saved
 :repeat
-youtube-dl -u "1234@gmail.com" -p "1234" https://www.udemy.com/learn-ethical/learn/v4/content || goto :repeat
+youtube-dl -u "1234@gmail.com" -p "1234pass" https://www.udemy.com/learn-ethical/learn/v4/content || goto :repeat
 echo Success!
 
 
