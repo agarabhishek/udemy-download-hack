2e440d8ebc33c16624f1f78b286fe1fd3bbf486b
271098jarvis <47479686+271098jarvis@users.noreply.github.com>
2019-08-22 22:00:23 +0530

    Update repeat.bat

--- repeat.bat
+++ repeat.bat
@@ -1,7 +1,7 @@
 pip install youtube-dl 
 cd ../../Users/Abhishek/Desktop/Vid rem Replace it with your location
 :repeat
-youtube-dl -u "1234@gmail.com" -p "1234pass" https://www.udemy.com/learn-ethical/learn/v4/content || goto :repeat
+youtube-dl -u "baniyashikhar@gmail.com" -p "Sukrulal" https://www.udemy.com/automatingetl/ || goto :repeat
 echo Success!
 
 
